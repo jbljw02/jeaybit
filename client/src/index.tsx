cd212e5c5119ba7d1555f93f408697d3778c4339
Jinu <jbljw02@naver.com>
2026-02-21 23:57:41 +0900

    fix: unused-vars

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,7 +3,6 @@ import './index.css';
 import App from './components/App';
 import { Provider } from 'react-redux';
 import store from './redux/store';
-import axios from './api/axios';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
